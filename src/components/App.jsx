18590d63dda49f5605cba0d80a81f6134184663b
EmiliaTschanter <tschanter@ruffstone.de>
2022-01-31 13:33:32 +0100

    update image dependencies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import "../styles/index.scss";
 import Header from "./Header";
-import wheat from "../images/wheat.svg";
+import wheat from "../images/crop-choice/wheat.svg";
 
 const App = () => {
     return (
