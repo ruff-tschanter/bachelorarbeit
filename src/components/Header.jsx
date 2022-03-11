9c93c8945934833cfafe8ce4e3c4a6ac664eef52
EmiliaTschanter <tschanter@ruffstone.de>
2022-03-11 16:26:31 +0100

    slightly update styles

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import pin from "../images/pin.svg";
-import sun from "../images/sun.svg";
-import drop from "../images/waterdrop.svg";
-import settings from "../images/settings.svg";
-
-const Header = () => {
-};
-
-export default Header;
