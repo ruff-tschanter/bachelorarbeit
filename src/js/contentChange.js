19cadec958010e86fec53e9387cbed70038a5255
EmiliaTschanter <tschanter@ruffstone.de>
2022-03-18 11:32:48 +0100

    center headlines in navi

--- src/js/contentChange.js
+++ src/js/contentChange.js
@@ -3,6 +3,6 @@ var fieldNumber = 1
 changeFieldNumber = function() {
   fieldNumber++;
 
-  document.getElementsByClassName('changeFieldNumber')[0].innerHTML=fieldNumber;
-  document.getElementsByClassName('changeFieldNumber')[1].innerHTML="(F"+fieldNumber+")";
+  document.getElementsByClassName('changeFieldNumber')[0].innerHTML="F"+fieldNumber;
+  document.getElementsByClassName('changeFieldNumber')[1].innerHTML="F"+fieldNumber;
 }
